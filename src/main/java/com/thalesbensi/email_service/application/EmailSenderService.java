b22261af6976f5c92f58a7309547eb7c02bb379c
thalesbensi <thalesbensi@outlook.com>
2025-05-23 09:36:13 -0300

    feat: Implement email sender service in application layer

---
+++ src/main/java/com/thalesbensi/email_service/application/EmailSenderService.java
@@ -0,0 +1,18 @@
+package com.thalesbensi.email_service.application;
+
+import com.thalesbensi.email_service.adapters.EmailSenderAdapter;
+import com.thalesbensi.email_service.core.usecases.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderAdapter emailSenderAdapter;
+
+    public EmailSenderService(EmailSenderAdapter emailSenderAdapter) {
+        this.emailSenderAdapter = emailSenderAdapter;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        emailSenderAdapter.sendEmail(to, subject, body);
+    }
+}
